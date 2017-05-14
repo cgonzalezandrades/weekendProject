020489eba346600a5c3e293c50e5a28b72d048cc
Carlos Gonzalez <cubeton@gmail.com>
2017-05-13 21:49:23 -0500

    restore sendgrid

--- server.js
+++ server.js
@@ -20,31 +20,31 @@ app.post('/', function (req, res, next) {
     console.log(req.body);
 
 
-    // var helper = require('sendgrid').mail;
-    // var fromEmail = new helper.Email(req.body.email);
-    // var toEmail = new helper.Email('morandinfo@morandtransportation.com');
-    // var subject = 'Requesting info';
-    // var content = new helper.Content('text/plain', 'Name: '+ req.body.firstName +' '+ req.body.lastName + '<br>'+
-    //     'Phone Number: '+ req.body.phoneNumber +
-    //     req.body.message);
-    //
-    // var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-    //
-    // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    // var request = sg.emptyRequest({
-    //     method: 'POST',
-    //     path: '/v3/mail/send',
-    //     body: mail.toJSON()
-    // });
-    //
-    // sg.API(request, function (error, response) {
-    //     if (error) {
-    //         console.log('Error response received');
-    //     }
-    //     console.log(response.statusCode);
-    //     console.log(response.body);
-    //     console.log(response.headers);
-    // });
+    var helper = require('sendgrid').mail;
+    var fromEmail = new helper.Email(req.body.email);
+    var toEmail = new helper.Email('morandinfo@morandtransportation.com');
+    var subject = 'Requesting info';
+    var content = new helper.Content('text/plain', 'Name: '+ req.body.firstName +' '+ req.body.lastName + '<br>'+
+        'Phone Number: '+ req.body.phoneNumber +
+        req.body.message);
+
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON()
+    });
+
+    sg.API(request, function (error, response) {
+        if (error) {
+            console.log('Error response received');
+        }
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });
 
     // res.end('yes')
 })
